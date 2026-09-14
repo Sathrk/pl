245230bc382a0ab4823f2679c9fd4f7ec31a31fa
sarthk <117702491+Sathrk@users.noreply.github.com>
2026-09-14 09:53:30 +0530

    Refactor ChatManager into ChatListener for event handling

--- src/main/java/com/yourserver/webbridge/listener/ChatListener.java
+++ src/main/java/com/yourserver/webbridge/listener/ChatListener.java
@@ -1,24 +1,15 @@
-package com.yourpackage.webserver.manager;
+package com.yourpackage.webserver.listener;
 
-import java.util.LinkedList;
-import java.util.List;
+import com.yourpackage.webserver.manager.ChatManager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-public class ChatManager {
-    private static final LinkedList<String> chatLogs = new LinkedList<>();
-    private static final int MAX_LOGS = 50;
-
-    public static synchronized void addMessage(String player, String message) {
-        String jsonMsg = String.format("{\"player\":\"%s\",\"message\":\"%s\"}", 
-            player.replace("\"", "\\\""), 
-            message.replace("\"", "\\\"")
-        );
-        chatLogs.add(jsonMsg);
-        if (chatLogs.size() > MAX_LOGS) {
-            chatLogs.removeFirst();
-        }
-    }
-
-    public static synchronized String getChatJson() {
-        return "[" + String.join(",", chatLogs) + "]";
+public class ChatListener implements Listener {
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+        if (event.isCancelled()) return;
+        ChatManager.addMessage(event.getPlayer().getName(), event.getMessage());
     }
 }
